bd26d7a98673aaaab7609e8b488b7e7a64f45711
p-ranoid <nmirandan98@gmail.com>
2025-05-30 01:51:05 -0500

    Create 1.cpp

---
+++ 181 excersises/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, suma;
+    cout << "Ingrese el primer número: ";
+    cin >> num1;
+    cout << "Ingrese el segundo número: ";
+    cin >> num2;
+    suma = num1 + num2;
+    cout << "La suma es: " << suma << endl;
+    return 0;
+}
