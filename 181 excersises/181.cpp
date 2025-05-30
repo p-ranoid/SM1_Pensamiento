06e33a518563761ce868d2f4586c3dc7895f7e24
p-ranoid <nmirandan98@gmail.com>
2025-05-30 02:10:37 -0500

    Add files via upload

--- 181 excersises/181.cpp
+++ 181 excersises/181.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main() {
     int n;
-    cout << "Ingrese el tamaño de la lista: ";
+    cout << "Ingresa el tamaño de la lista: ";
     cin >> n;
     int *arr = new int[n];
-    cout << "Ingresa los " << n << " valores del arreglo:\n";
+    cout << "Ingresa los " << n << " valores de la lista:\n";
     for (int i = 0; i < n; i++) {
-        cout << "valor " << i + 1 << ": ";
+        cout << "Valor " << i + 1 << ": ";
         cin >> arr[i];
     }
     cout << "\nLista ingresada:\n";
